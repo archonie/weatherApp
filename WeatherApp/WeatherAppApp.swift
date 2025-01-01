74c4a651bb78092d3a5d7535bcfc4eb5432df525
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-01-01 22:24:35 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 1.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
